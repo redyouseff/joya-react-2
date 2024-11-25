35940c1413a0b1715ae692f9c6a6e0b72df685a2
redyouseff <yousefalkret2@gmail.com>
2024-11-25 19:58:10 +0200

    v4

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#041d1a] text-[#f4f3ef] py-10">
       <div className="container mx-auto px-6 lg:px-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 items-start justify-items-center text-center lg:text-left">
           {/* Brand Information */}
           <div className="flex flex-col space-y-4">
             <h3 className="text-2xl font-extrabold text-[#a0b3b1] mb-4">
@@ -63,7 +63,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          {/* Contact Information and Social Media */}
+          {/* Contact Information */}
           <div>
             <h4 className="text-lg font-semibold mb-4 text-[#a0b3b1]">
               Contact Us
@@ -71,13 +71,12 @@ const Footer = () => {
             <p className="text-base text-[#f0ede6]/80 mb-1">
               Dubai, Business Bay, Prime Tower 1401
             </p>
-            <p className="text-base text-[#f0ede6]/80 mb-1">
-              Phone: +971 4 765 4321
-            </p>
+            <p className="text-base text-[#f0ede6]/80 mb-1">Phone: +971 4 765 4321</p>
             <p className="text-base text-[#f0ede6]/80 mb-4">
               Email: info@joyaproperties.com
             </p>
-            <div className="flex flex-wrap justify-start md:justify-start lg:justify-start gap-4">
+            {/* Social Media Links */}
+            <div className="flex space-x-4 justify-center lg:justify-start">
               {socialMediaLinks.map(({ icon: Icon, href }, index) => (
                 <a
                   key={index}
